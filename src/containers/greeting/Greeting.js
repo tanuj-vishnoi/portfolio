159814b886e4ad1f0a74c5d7d32b21cf8b491d76
tanuj <tanujvishnoi@gmail.com>
2025-05-14 22:40:21 +0200

    changed data

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -42,9 +42,10 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume.pdf")}
+                    href={greeting.resumeLink}
                     download="Resume.pdf"
                     className="download-link-button"
+                    target="_blank" rel="noreferrer"
                   >
                     <Button text="Download my resume" />
                   </a>
